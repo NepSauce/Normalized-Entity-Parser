d92a3233987f29b175b8c93644f2183c5e8935a1
NepSauce <greasyandmoist@gmail.com>
2025-03-05 23:54:31 -0400

    Added Data Points in RosterEntityDetails.java.

--- app/src/main/java/nep/entityclass/RosterEntityDetails.java
+++ app/src/main/java/nep/entityclass/RosterEntityDetails.java
@@ -1,15 +1,49 @@
 package nep.entityclass;
 
-public class RosterEntityDetails {
-    private int day;
-    private String month;
-    private int year;
-    private String location;
+@SuppressWarnings("unused")
+public class RosterEntityDetails{
+    private String INPUT_DIRECTORY = "";
+    private final String LOCATION;
+    private final int DAY;
+    private final String MONTH;
+    private final int YEAR;
 
     public RosterEntityDetails(String location, int day, String month, int year){
-        this.location = location;
-        this.day = day;
-        this.month = month;
-        this.year = year;
+        this.LOCATION = location;
+        this.DAY = day;
+        this.MONTH = month;
+        this.YEAR = year;
+    }
+
+    public RosterEntityDetails(String directory, String location, int day, String month, int year){
+        this.INPUT_DIRECTORY = directory;
+        this.LOCATION = location;
+        this.DAY = day;
+        this.MONTH = month;
+        this.YEAR = year;
+    }
+
+    public String getLOCATION(){
+        return LOCATION;
+    }
+
+    public int getDAY(){
+        return DAY;
+    }
+
+    public String getMONTH(){
+        return MONTH;
+    }
+
+    public int getYEAR(){
+        return YEAR;
+    }
+
+    public String getDirectory(){
+        return INPUT_DIRECTORY;
+    }
+
+    public void setDirectory(String newDirectory){
+        INPUT_DIRECTORY = newDirectory;
     }
 }
