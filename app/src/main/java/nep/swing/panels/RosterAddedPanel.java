9e51ba082160b468238c7063c80c4a8001395168
Zawad Atif <greasyandmoist@gmail.com>
2025-03-10 12:24:08 -0300

    Chore: Addedd Height and Width specifications for RosterAddedPanel.java.

---
+++ app/src/main/java/nep/swing/panels/RosterAddedPanel.java
@@ -0,0 +1,16 @@
+package nep.swing.panels;
+
+import javax.swing.JPanel;
+
+public class RosterAddedPanel{
+    private JPanel rosterBrowsingPanel;
+    private int browsingPanelHeight;
+    private int browsingPanelWidth;
+
+    public RosterAddedPanel(){
+        browsingPanelHeight = 50;
+        browsingPanelWidth = 250;
+
+        
+    }
+}
