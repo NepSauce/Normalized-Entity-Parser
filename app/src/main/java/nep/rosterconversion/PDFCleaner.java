14b93ef73dc2225b70c172d1c40d81bcd7ab5913
Nafisah Nubah <Nafisah.Nubah@dal.ca>
2025-04-18 19:37:52 -0300

    Feat: Added additional fields to the grouped appointments

--- app/src/main/java/nep/rosterconversion/PDFCleaner.java
+++ app/src/main/java/nep/rosterconversion/PDFCleaner.java
@@ -4,6 +4,18 @@ import java.io.*;
 import java.util.*;
 
 public class PDFCleaner {
+    private static final String[] METADATA_FIELDS = {
+        "Duration:",
+        "Permitted Materials:",
+        "Exam Source:",
+        "Medium:",
+        "PU/DEL:",
+        "Password:",
+        "Notes:"
+    };
+    
+    private static final int COURSE_CODE_WIDTH = 50;
+
     public static void main(String[] args) {
         String inputFilePath = "Media/output.txt";
         String outputFilePath = "Media/filtered_appointments.txt";
@@ -16,7 +28,7 @@ public class PDFCleaner {
              BufferedWriter filteredWriter = new BufferedWriter(new FileWriter(outputFilePath));
              BufferedWriter groupedWriter = new BufferedWriter(new FileWriter(groupedFilePath))) {
             
-            Map<String, Map<String, Integer>> courseGroups = new TreeMap<>(); // Course -> (Time -> Count)
+            Map<String, Map<String, Integer>> courseGroups = new TreeMap<>();
             String line;
 
             while ((line = reader.readLine()) != null) {
@@ -27,23 +39,35 @@ public class PDFCleaner {
                     String originalCourse = parts[2].trim();
                     String normalizedCourse = normalizeCrossListedCourse(originalCourse);
                     
-                    // Write to filtered appointments
                     filteredWriter.write(String.format("Time: %s, Course Code: %s%n", time, originalCourse));
                     
-                    // Group by course and time
                     courseGroups
                         .computeIfAbsent(normalizedCourse, k -> new TreeMap<>())
                         .merge(time, 1, Integer::sum);
                 }
             }
 
-            // Write grouped appointments
             for (Map.Entry<String, Map<String, Integer>> courseEntry : courseGroups.entrySet()) {
-                groupedWriter.write(String.format("Course Code: %s%n", courseEntry.getKey()));
+                String courseLine = String.format("Course Code: %s", courseEntry.getKey());
+                groupedWriter.write(padRight(courseLine, COURSE_CODE_WIDTH) + METADATA_FIELDS[0] + "\n");
                 
+                boolean firstTimeEntry = true;
                 for (Map.Entry<String, Integer> timeEntry : courseEntry.getValue().entrySet()) {
-                    groupedWriter.write(String.format("%d - [location] - %s%n", 
-                                      timeEntry.getValue(), timeEntry.getKey()));
+                    String timeLine = String.format(" %d - [location] - %s", 
+                                      timeEntry.getValue(), timeEntry.getKey());
+                    groupedWriter.write(padRight(timeLine, COURSE_CODE_WIDTH));
+                    
+                    if (firstTimeEntry) {
+                        for (int i = 1; i < METADATA_FIELDS.length; i++) {
+                            groupedWriter.write(METADATA_FIELDS[i] + "\n");
+                            if (i < METADATA_FIELDS.length - 1) {
+                                groupedWriter.write(padRight("", COURSE_CODE_WIDTH));
+                            }
+                        }
+                        firstTimeEntry = false;
+                    } else {
+                        groupedWriter.write("\n");
+                    }
                 }
                 groupedWriter.newLine();
             }
@@ -63,5 +87,9 @@ public class PDFCleaner {
             return String.join("/", departments) + " " + parts[1];
         }
         return courseCode;
+    }
+
+    private static String padRight(String s, int width) {
+        return String.format("%-" + width + "s", s);
     }
 }
