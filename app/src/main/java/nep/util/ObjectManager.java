1808fe929200f20a69ce231a27cb80de1ceb6722
NepSauce <greasyandmoist@gmail.com>
2025-05-02 22:21:10 -0300

    Fix: Fixed method in ObjectManager.java to set value to null instead of empty.

--- app/src/main/java/nep/util/ObjectManager.java
+++ app/src/main/java/nep/util/ObjectManager.java
@@ -28,21 +28,17 @@ public class ObjectManager {
         int start = object.indexOf(pattern);
         if (start == -1) return object;
         
-        int end = object.indexOf(" |", start);
-        if (end == -1) {
-            // last element before closing ]
-            end = object.lastIndexOf("]");
-            if (object.charAt(start - 1) == '|') start -= 2; // remove preceding delimiter
-            else if (object.charAt(start - 1) == ' ') start -= 1;
-        }
+        int valueStart = start + pattern.length();
+        int valueEnd = object.indexOf(" |", valueStart);
+        if (valueEnd == -1) valueEnd = object.indexOf("]", valueStart);
         
-        object = object.substring(0, start) + object.substring(end + 1);
-        object = object.replaceAll("\\[\\s*\\|", "["); // clean leading "|"
-        object = object.replaceAll("\\|\\s*\\]", "]"); // clean trailing "|"
+        if (valueEnd == -1) return object;
+        
+        object = object.substring(0, valueStart) + "null" + object.substring(valueEnd);
         return object.trim();
     }
     
-    public String addIfNull(String key, String value) {
+    public String addIfKeyValueNull(String key, String value) {
         String pattern = key + ": ";
         int start = object.indexOf(pattern);
         if (start == -1) return object;
